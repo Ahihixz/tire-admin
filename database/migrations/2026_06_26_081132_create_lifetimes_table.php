24fad984526b96be94a2cbb493235dbf843707cc
kuple <kuple@kali.kuple>
2026-06-27 13:00:05 +0700

    Ready for Render

--- database/migrations/2026_06_26_081132_create_lifetimes_table.php
+++ database/migrations/2026_06_26_081132_create_lifetimes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateLifetimesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -24,4 +24,4 @@ return new class extends Migration
     {
         Schema::dropIfExists('lifetimes');
     }
-};
+}
